6f7202348825aee5a06b62b6afe32bf81b0e3886
DaniSode <danisode@student.chalmers.se>
2023-05-25 14:54:12 +0200

    gfdg

--- main2.m
+++ main2.m
@@ -18,7 +18,7 @@ orient = meas.orient;
 t_meas = meas.t;
 
 % Convert to euler angles
-real_angles = q2euler(orient)*(pi/180);
+real_angles = q2euler(orient);
 
 % Save states and covariance
 x = xhat.x;
@@ -26,7 +26,7 @@ P = xhat.P;
 t_xhat = xhat.t;
 
 % Convert to euler angles
-est_angles = q2euler(x);
+est_angles = q2euler(x)*(180/pi);
 
 %% Plot 
 size = get(0,'screensize'); size = size(1,end-1:end);
@@ -36,17 +36,17 @@ subplot(3,1,1)
 plot(t_xhat, est_angles(1,:))
 hold on; grid on
 plot(t_meas, real_angles(1,:))
-title('Roll, real vs estimation'); xlabel('Time [s]'); ylabel('Angle [rad]')
+title('Roll, real vs estimation'); xlabel('Time [s]'); ylabel('Angle [degrees]')
 legend('Estimated', 'Real')
 subplot(3,1,2)
 plot(t_xhat, est_angles(2,:))
 hold on; grid on
 plot(t_meas, real_angles(2,:))
-title('Pitch, real vs estimation'); xlabel('Time [s]'); ylabel('Angle [rad]')
+title('Pitch, real vs estimation'); xlabel('Time [s]'); ylabel('Angle [degrees]')
 legend('Estimated', 'Real')
 subplot(3,1,3)
 plot(t_xhat, est_angles(3,:))
 hold on; grid on
 plot(t_meas, real_angles(3,:))
-title('Yaw, real vs estimation'); xlabel('Time [s]'); ylabel('Angle [rad]')
+title('Yaw, real vs estimation'); xlabel('Time [s]'); ylabel('Angle [degrees]')
 legend('Estimated', 'Real')
